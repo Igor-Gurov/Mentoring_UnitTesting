a1a861406b151fdbb4662236cd0093186f4e26fe
Igor-Gurov <igor0gurov@gmail.com>
2013-11-15 16:32:48 +0400

    Add new proxy server

---
+++ Proxy/Proxy/MainPage.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainPage.aspx.cs" Inherits="Proxy.MainPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+		<input id="inpAddress" runat="server" />
+    </div>
+	<div>
+		<asp:Button ID="btnConfirm" runat="server" Text="Confirm" OnClick="btnConfirm_Click" />
+	</div>
+    </form>
+</body>
+</html>
---
+++ Proxy/Proxy/ProxyHttpHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.SessionState;
+
+namespace Proxy
+{
+	public class ProxyHttpHandler : IHttpHandler, IRequiresSessionState
+	{
+		public ProxyHttpHandler()
+		{
+
+		}
+
+		public bool IsReusable
+		{
+			get
+			{
+				return false;
+			}
+		}
+
+		public void ProcessRequest(HttpContext context)
+		{
+			string query;
+				query = context.Request.Url.AbsoluteUri;
+			
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(query);
+			//context.Session["Responce"] = request.GetResponse();
+			HttpWebResponse responce = (HttpWebResponse)request.GetResponse();
+			Stream stream = responce.GetResponseStream();
+			Encoding encode = Encoding.GetEncoding(responce.CharacterSet);
+			StreamReader readStream = new StreamReader(stream, encode);
+			Char[] readChar = new Char[256];
+			int count = readStream.Read(readChar, 0, 256);
+			while (count > 0)
+			{
+				context.Response.Write(new string(readChar, 0, count));
+				count = readStream.Read(readChar, 0, 256);
+			}
+			readStream.Close();
+			responce.Close();
+			stream.Close();
+		}
+	}
+}
